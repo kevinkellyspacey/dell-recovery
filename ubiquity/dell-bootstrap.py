03e38059def612745a31620bd4065ec70726a939
= <kevin_rustin_wang@dell.com>
2016-04-14 18:39:29 +0800

    fix the swap partition delete issue

--- ubiquity/dell-bootstrap.py
+++ ubiquity/dell-bootstrap.py
@@ -449,7 +449,10 @@ class Page(Plugin):
         try:
             os_path = magic.fetch_output(['readlink','/dev/disk/by-label/'+label]).split('\n')            
         except Exception as err:
-            self.log('os_path command is executed failed, the error is %s'%str(err))
+            # compatible with DUALSYS partition label when boot from hdd 
+            os_path = magic.fetch_output(['readlink','/dev/disk/by-label/UBUNTU']).split('\n')
+            if not os_path:
+                self.log('os_path command is executed failed, the error is %s'%str(err))
        
         os_part = digits.search(os_path[0].split('/')[-1]).group()
 
@@ -933,7 +936,6 @@ class RPbuilder(Thread):
 
     def build_rp(self, cushion=600):
         """Copies content to the recovery partition using a parted wrapper.
-
            This might be better implemented in python-parted or parted_server/partman,
            but those would require extra dependencies, and are generally more complex
            than necessary for what needs to be accomplished here."""
@@ -1345,6 +1347,14 @@ class Install(InstallPlugin):
 
         #install dell-recovery only if there is an RP
         if rec_part:
+            #hide the recovery partition as default
+            try:
+                recovery = magic.find_factory_partition_stats()
+                command = ('parted', '-a', 'optimal', '-s', recovery['slave'], 'set', str(recovery['number']), 'msftres', 'on' )
+                misc.execute_root(*command)                
+            except Exception:
+                pass
+            
             to_install.append('dell-recovery')
             to_install.append('dell-eula')
 
@@ -1387,4 +1397,3 @@ class Install(InstallPlugin):
         install_misc.record_removed(to_remove)
 
         return InstallPlugin.install(self, target, progress, *args, **kwargs)
-
